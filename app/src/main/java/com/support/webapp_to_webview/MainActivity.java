54583d5607632334a14c7d9e8fb665c839df54ba
aaronbudig <aaronbudig@bymason.com>
2025-07-31 15:10:38 -0700

    initial commit

---
+++ app/src/main/java/com/support/webapp_to_webview/MainActivity.java
@@ -0,0 +1,75 @@
+package com.support.webapp_to_webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startWebView("https://ap-iwa.com/ProMobileV3"); // Update me to the URL
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void startWebView(String url) {
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        myWebView= findViewById(R.id.webview);
+        WebSettings webSettings=myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (progressBar.isShown()) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(MainActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        myWebView.loadUrl(url);
+    }
+}
+
